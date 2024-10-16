eb125146dd1d95b8b5267227cf244f826905e7b3
Marat <msaltysh@gmail.com>
2024-10-16 17:58:53 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    # equls = 0
+    if second == 0:
+        return 'Ошибка!'
+    else:
+        return first / second
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(first, second):
+   # equls = 0
+    if second == 0:
+        return inf
+    else:
+        return first / second
